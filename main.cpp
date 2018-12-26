5dee813e9db080345c11841a30424571ad29563a
Ahmed Hammad <hammad97official@gmail.com>
2018-12-27 01:13:53 +0200

    Added LRU

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <limits>
 
 using namespace std;
 
@@ -17,9 +20,24 @@ bool isThere(vector<int>& pages, int target){
     return false;
 }
 
-void firstInFirstOut(vector<int>& pages){
-int page, oldest=0, falts=0;
+int findAndReplaceMinimum(unordered_map<int,int>& usage, int page){
+    int minValue = numeric_limits<int>::max();
+    int minKey = -1;
 
+    for(auto record : usage){
+        if(record.second < minValue){minValue = record.second; minKey = record.first;}
+    }
+    return minKey;
+}
+
+void replacePage(vector<int>& pages, int oldPage, int newPage){
+    for(auto& page : pages){
+        if(page==oldPage){page = newPage; return;}
+    }
+}
+
+void firstInFirstOut(vector<int>& pages){
+    int page, oldest=0, faults=0;
     while(1){
         cin>>page;
         if(page==-1){return;}
@@ -27,6 +45,7 @@ int page, oldest=0, falts=0;
         if(pages.size() == pages.capacity()){
             pages[oldest] = page;
             oldest++;
+            faults++;
             if(oldest == pages.size()){oldest=0;}
         }
         else{pages.push_back(page);}
@@ -34,7 +53,24 @@ int page, oldest=0, falts=0;
 }
 
 void leastRecentlyUsed(vector<int>& pages){
-
+    int page, faults=0, index=0;
+    unordered_map<int,int> usage;
+    while(1){
+        cin>>page;
+        if(page==-1){return;}
+        if(isThere(pages, page)){usage[page] = ++index; continue;}
+        if(pages.size() == pages.capacity()){
+            int minn = findAndReplaceMinimum(usage, page);
+            usage.erase(minn);
+            usage[page] = ++index;
+            replacePage(pages, minn, page);
+            faults++;
+        }
+        else{
+        pages.push_back(page);
+        usage[page] = ++index;
+        }
+    }
 }
 
 void clock(vector<int>& pages){
